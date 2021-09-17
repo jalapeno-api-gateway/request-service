d2a5bb00393d4d768c56bef3401df65a8a213dec
Dominique <illi.dominique.v@gmail.com>
2021-09-17 11:44:55 +0200

    added max and min time to influx query to increase performance

--- influxdb/fetch.go
+++ influxdb/fetch.go
@@ -8,17 +8,17 @@ import (
 )
 
 const (
-	InterfaceNameIdentifier = "interface_name"
-	IpAddressIdentifier = "ip_information/ip_address"
-	DataRateIdentifier = "data_rates/output_data_rate"
-	PacketsSentIdentifier = "interface_statistics/full_interface_stats/packets_sent"
-	PacketsReceivedIdentifier = "interface_statistics/full_interface_stats/packets_received"
-	StateIdentifier = "state"
+	InterfaceNameIdentifier           = "interface_name"
+	IpAddressIdentifier               = "ip_information/ip_address"
+	DataRateIdentifier                = "data_rates/output_data_rate"
+	PacketsSentIdentifier             = "interface_statistics/full_interface_stats/packets_sent"
+	PacketsReceivedIdentifier         = "interface_statistics/full_interface_stats/packets_received"
+	StateIdentifier                   = "state"
 	LastStateTransitionTimeIdentifier = "last_state_transition_time"
 )
 
 func FetchDataRate(ipv4address string) (int64, error) {
-	queryString := fmt.Sprintf("select last(\"%s\") FROM \"Cisco-IOS-XR-pfi-im-cmd-oper:interfaces/interface-xr/interface\" WHERE \"ip_information/ip_address\" = '%s'", DataRateIdentifier, ipv4address)
+	queryString := fmt.Sprintf("select last(\"%s\") FROM \"Cisco-IOS-XR-pfi-im-cmd-oper:interfaces/interface-xr/interface\" WHERE \"ip_information/ip_address\" = '%s' AND time < now() and time > now() - 5m", DataRateIdentifier, ipv4address)
 	response := queryInflux(queryString)
 
 	if len(response.Results[0].Series) == 0 {
