c724fd6f0f43883e2c10e714e1c10d743dc4b169
Michel Bongard <michel_bongard@hotmail.com>
2021-10-15 09:14:04 +0200

    Revert "logging to console to test deployment"
    This reverts commit 12b3680baa86406cd381e47a6d95be1c6490023b.

--- influxdb/connect.go
+++ influxdb/connect.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	influx "github.com/influxdata/influxdb1-client/v2"
 )
@@ -12,11 +11,6 @@ import (
 var InfluxClient *influx.Client
 
 func InitializeInfluxClient() {
-	log.Printf("InitializeInfluxClient - INFLUX_ADDRESS: %s", os.Getenv("INFLUX_ADDRESS"))
-	log.Printf("InitializeInfluxClient - INFLUX_USER: %s", os.Getenv("INFLUX_USER"))
-	log.Printf("InitializeInfluxClient - INFLUX_PASSWORD: %s", os.Getenv("INFLUX_PASSWORD"))
-	log.Printf("InitializeInfluxClient - INFLUX_DB: %s", os.Getenv("INFLUX_DB"))
-
 	client, err := influx.NewHTTPClient(influx.HTTPConfig{
 		Addr:     fmt.Sprintf("http://%s", os.Getenv("INFLUX_ADDRESS")),
 		Username: os.Getenv("INFLUX_USER"),
@@ -26,19 +20,10 @@ func InitializeInfluxClient() {
 		//TODO: Inform SR-App about unavailable
 		log.Fatal("Error creating InfluxDB Client: ", err.Error())
 	}
-
-	_, _, err = client.Ping(time.Second * 10)
-
-	if err != nil {
-		log.Printf("Could not ping: %s", err)
-	}
-
 	InfluxClient = &client
 }
 
 func queryInflux(queryString string) *influx.Response {
-	log.Printf("queryInflux - INFLUX_DB: %s", os.Getenv("INFLUX_DB"))
-
 	query := influx.NewQuery(queryString, os.Getenv("INFLUX_DB"), "")
 	response, err := (*InfluxClient).Query(query)
 
@@ -46,7 +31,7 @@ func queryInflux(queryString string) *influx.Response {
 		log.Fatalf("Error querying InfluxDb: %v", err)
 	}
 	if response.Error() != nil {
-		log.Fatalf("(Response-)Error querying InfluxDb: %v", response.Error())
+		log.Fatalf("Error querying InfluxDb: %v", response.Error())
 	}
 	return response
 }
